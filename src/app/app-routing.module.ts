8667fb1ae03f30a46ccf34c9ad897b0c8d5d76cd
sweting <sebastian.strittmatter@sweting.de>
2021-12-23 19:25:19 +0000

    ADD: Frame-Option. Not finished yet.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,8 @@ const routes: Routes = [
   { path: 'lframe', loadChildren: () => import('./frame/frame.module').then(m => m.FrameModule) },
   { path: 'lconfiguration', loadChildren: () => import('./configuration/configuration.module').then(m => m.ConfigurationModule) },
   { path: ':station_name', component: OverviewComponent, data: { icecastMeta: false } },
+  { path: ':station_name/frame', component: OverviewComponent, data: { icecastMeta: false, isFrame: true } },
+  { path: ':station_name/frame/:config', component: OverviewComponent, data: { icecastMeta: false, isFrame: true } },
   { path: ':station_name/direct', component: OverviewComponent, data: { icecastMeta: true } },
   { path: ':station_name/:config', component: OverviewComponent, data: {icecastMeta: false} },
 ];
